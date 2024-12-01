893ae777ab07ddaff519f4e8d27eb6009d96efe5
Adrian <adriansanduta@gmail.com>
2024-11-30 19:45:52 -0600

    2.8.1

--- app.js
+++ app.js
@@ -86,3 +86,10 @@ function reverseString(str) {
 }
 console.log(reverseString("This is cool"));
 
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+console.log(reverseString("This is cool"));
+
+// 9.
