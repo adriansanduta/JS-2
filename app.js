988fd70fdeb84a376948a5278fa7db628cab3374
Adrian <adriansanduta@gmail.com>
2024-11-30 18:21:22 -0600

    2.2

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ function filterOutFalsy(elem1, elem2) {
 }
 console.log(filterOutFalsy(0, 50));
 
+// 2.Return the length of any given array
+// Given an array, return it's length
+
+function arrLength(arr) {
+     return arr.length;
+}
+console.log(arrLength([1, 2, 3, 4]));
+
